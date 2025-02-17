05d03ba3b9d5976e7ae82b57320c6b1da2a3f932
HeeJae <132039586+1026hz@users.noreply.github.com>
2025-02-18 08:49:59 +0900

    [Bronze V] Title: 새싹, Time: 104 ms, Memory: 14052 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void solution() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        System.out.println("         ,r'\"7\n" +
+                "r`-_   ,'  ,/\n" +
+                " \\. \". L_r'\n" +
+                "   `~\\/\n" +
+                "      |\n" +
+                "      |");
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        solution();
+    }
+}
