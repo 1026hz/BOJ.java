dd305c23f6a2faaa833976263388e367e686bfd8
HeeJae <132039586+1026hz@users.noreply.github.com>
2025-02-17 12:49:25 +0900

    [Bronze V] Title: 개, Time: 100 ms, Memory: 14024 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void solution() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|");
+    }
+
+    public static void main(String[] args) throws IOException {
+        solution();
+    }
+}
