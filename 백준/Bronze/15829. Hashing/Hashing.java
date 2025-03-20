42d326942d8e7848bce268b153770905a38cbf88
HeeJae <132039586+1026hz@users.noreply.github.com>
2025-03-20 09:12:28 +0900

    [Bronze II] Title: Hashing, Time: 104 ms, Memory: 14360 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -1,28 +1,27 @@
-import java.util.*;
 import java.io.*;
 
 public class Main{
 
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int r = 31;
-        int m = 1234567891;
-        String alphabet = "0abcdefghijklmnopqrstuvwxyz";
+        long r = 31;
+        long m = 1234567891;
 
         int n = Integer.parseInt(br.readLine());
         String str = br.readLine();
-        int ans = 0;
-        int tmp = 1;
 
-        for (int i=0; i<n; i++){
-            ans += (alphabet.indexOf(str.charAt(i)) * tmp);
-            tmp *= r;
+        long ans = 0;
+        long hash = 1;
+
+        for (int i = 0; i < n; i++) {
+            ans += ((long) (str.charAt(i) - 96) * hash);
+            hash = (hash * r) % m;
         }
 
-        bw.write(ans + "");
+        bw.write(ans % m + "");
         bw.flush();
         bw.close();
         br.close();
